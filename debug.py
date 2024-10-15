2538971efaa16a8d2f71de383f0f3669c8d07c4d
Luiza <luizahelena.lsantos@gmail.com>
2024-10-15 11:59:11 -0300

    more images

---
+++ debug.py
@@ -0,0 +1,6 @@
+import glob
+
+files = glob.glob('c:/Users/ALUNO SESI E SENAI.MATRIZ/Desktop/projeto1 VC/images/*')
+
+for f in files:
+    print(f)
--- main.py
+++ main.py
@@ -2,37 +2,64 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 from path import path
+import glob
+
+# Array de tuples com as imagens editadas
+imageArr = []
 
 # Read image 'image1.jpg' as grayscale
-img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+for file in glob.glob(path):
+    img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
 
-_, img_thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+    _, img_thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
 
-img_inverted = 255 - img
+    img_inverted = 255 - img
 
-# Equalizar o histograma
-img_eq = cv2.equalizeHist(img)
+    # Equalizar o histograma
+    img_eq = cv2.equalizeHist(img)
 
-# Mostrar antes e depois
-plt.figure(figsize=(10, 5))
-plt.subplot(1, 4, 1)
-plt.imshow(img, cmap='gray')
+    imageArr.append((img, img_eq, img_thresh, img_inverted))
+
+# Criar a figura para exibir as imagens
+plt.figure(figsize=(15, 10))  # Ajuste o tamanho conforme necessário
+
+# Loop para exibir as imagens e suas edições
+plt.subplot(len(imageArr), 4, 1)
 plt.title('Imagem Original')
-plt.axis('off')
-
-plt.subplot(1, 4, 2)
-plt.imshow(img_eq, cmap='gray')
+plt.subplot(len(imageArr), 4, 2)
 plt.title('Imagem Equalizada')
-plt.axis('off')
-
-plt.subplot(1, 4, 3)
-plt.imshow(img_thresh, cmap='gray')
+plt.subplot(len(imageArr), 4, 3)
 plt.title('Imagem Binarizada')
-plt.axis('off')
-
-plt.subplot(1, 4, 4)
-plt.imshow(img_inverted, cmap='gray')
+plt.subplot(len(imageArr), 4, 4)
 plt.title('Imagem Invertida')
-plt.axis('off')
 
+for i, tup in enumerate(imageArr):
+    img, img_eq, img_thresh, img_inverted = tup
+
+    # Mostrar original
+    plt.subplot(len(imageArr), 4, 4 * i + 1)
+    plt.imshow(img, cmap='gray')
+    
+    plt.axis('off')
+
+    # Mostrar equalizada
+    plt.subplot(len(imageArr), 4, 4 * i + 2)
+    plt.imshow(img_eq, cmap='gray')
+    
+    plt.axis('off')
+
+    # Mostrar binarizada
+    plt.subplot(len(imageArr), 4, 4 * i + 3)
+    plt.imshow(img_thresh, cmap='gray')
+    
+    plt.axis('off')
+
+    # Mostrar invertida
+    plt.subplot(len(imageArr), 4, 4 * i + 4)
+    plt.imshow(img_inverted, cmap='gray')
+    
+    plt.axis('off')
+
+# Exibir todas as imagens
+plt.tight_layout()  # Melhora o espaçamento entre os subplots
 plt.show()
