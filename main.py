d81c737bb1f7bd5d31202caa2921ee031a6ea11c
Luiza <luizahelena.lsantos@gmail.com>
2024-10-22 11:44:03 -0300

    graphs

--- main.py
+++ main.py
@@ -1,61 +1,51 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-from path import path
 import glob
 
 # Array de tuples com as imagens editadas
 imageArr = []
 
 # Read image 'image1.jpg' as grayscale
+path = 'C:/Users/ALUNO SESI E SENAI.MATRIZ/Desktop/Luiza/images/*'
 for file in glob.glob(path):
     img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
-
-    _, img_thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
 
     img_inverted = 255 - img
 
     # Equalizar o histograma
     img_eq = cv2.equalizeHist(img)
 
-    imageArr.append((img, img_eq, img_thresh, img_inverted))
+    imageArr.append((img, img_eq, img_inverted))
 
 # Criar a figura para exibir as imagens
 plt.figure(figsize=(15, 10))  # Ajuste o tamanho conforme necessário
 
 # Loop para exibir as imagens e suas edições
-plt.subplot(len(imageArr), 4, 1)
+plt.subplot(len(imageArr), 3, 1)
 plt.title('Imagem Original')
-plt.subplot(len(imageArr), 4, 2)
+plt.subplot(len(imageArr), 3, 2)
 plt.title('Imagem Equalizada')
-plt.subplot(len(imageArr), 4, 3)
-plt.title('Imagem Binarizada')
-plt.subplot(len(imageArr), 4, 4)
+plt.subplot(len(imageArr), 3, 3)
 plt.title('Imagem Invertida')
 
 for i, tup in enumerate(imageArr):
-    img, img_eq, img_thresh, img_inverted = tup
+    img, img_eq, img_inverted = tup
 
     # Mostrar original
-    plt.subplot(len(imageArr), 4, 4 * i + 1)
+    plt.subplot(len(imageArr), 3, 3 * i + 1)
     plt.imshow(img, cmap='gray')
     
     plt.axis('off')
 
     # Mostrar equalizada
-    plt.subplot(len(imageArr), 4, 4 * i + 2)
+    plt.subplot(len(imageArr), 3, 3 * i + 2)
     plt.imshow(img_eq, cmap='gray')
     
     plt.axis('off')
 
-    # Mostrar binarizada
-    plt.subplot(len(imageArr), 4, 4 * i + 3)
-    plt.imshow(img_thresh, cmap='gray')
-    
-    plt.axis('off')
-
     # Mostrar invertida
-    plt.subplot(len(imageArr), 4, 4 * i + 4)
+    plt.subplot(len(imageArr), 3, 3 * i + 3)
     plt.imshow(img_inverted, cmap='gray')
     
     plt.axis('off')
---
+++ new.py
@@ -0,0 +1,50 @@
+import cv2
+import matplotlib.pyplot as plt
+
+# Caminho da imagem
+path = 'C:/Users/ALUNO SESI E SENAI.MATRIZ/Desktop/Luiza/histograma/images/image1.jpg'
+
+# Ler imagem em escala de cinza
+img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+# Inverter imagem
+img_inverted = 255 - img
+
+# Equalizar o histograma
+img_eq = cv2.equalizeHist(img)
+
+# Calcular os histogramas
+hist_img = cv2.calcHist([img], [0], None, [256], [0, 256])
+hist_img_inverted = cv2.calcHist([img_inverted], [0], None, [256], [0, 256])
+hist_img_eq = cv2.calcHist([img_eq], [0], None, [256], [0, 256])
+
+# Plotar os histogramas
+plt.figure(figsize=(12, 6))
+
+# Histograma da imagem original
+plt.subplot(1, 3, 1)
+plt.plot(hist_img, color='black')
+plt.title('Histograma - Imagem Original')
+plt.xlim([0, 255])  # Limitar o eixo x para garantir que comece em 0
+plt.ylim([0, max(hist_img) * 1.05])  # Ajustar o eixo y para não cortar o gráfico
+plt.gca().set_ylim(bottom=0)  # Garantir que o y comece em 0
+
+# Histograma da imagem invertida
+plt.subplot(1, 3, 2)
+plt.plot(hist_img_inverted, color='red')
+plt.title('Histograma - Imagem Invertida')
+plt.xlim([0, 255])
+plt.ylim([0, max(hist_img_inverted) * 1.05])
+plt.gca().set_ylim(bottom=0)
+
+# Histograma da imagem equalizada
+plt.subplot(1, 3, 3)
+plt.plot(hist_img_eq, color='blue')
+plt.title('Histograma - Imagem Equalizada')
+plt.xlim([0, 255])
+plt.ylim([0, max(hist_img_eq) * 1.05])
+plt.gca().set_ylim(bottom=0)
+
+# Mostrar os gráficos
+plt.tight_layout()
+plt.show()
