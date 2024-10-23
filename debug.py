fa6ef3fb2d9e8e3be181721401739878d8be5f1a
Luiza <luizahelena.lsantos@gmail.com>
2024-10-22 23:18:16 -0300

    final modifications

--- debug.py
+++
@@ -1,6 +0,0 @@
-import glob
-
-files = glob.glob('c:/Users/ALUNO SESI E SENAI.MATRIZ/Desktop/projeto1 VC/images/*')
-
-for f in files:
-    print(f)
--- main.py
+++ main.py
@@ -2,54 +2,91 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import glob
-from path import path
+import os
 
-# Array de tuples com as imagens editadas
-imageArr = []
+def plot_image_and_histogram(img, img_eq, img_inverted):
+    # Calcular os histogramas
+    hist_img = cv2.calcHist([img], [0], None, [256], [0, 256])
+    hist_img_eq = cv2.calcHist([img_eq], [0], None, [256], [0, 256])
+    hist_img_inverted = cv2.calcHist([img_inverted], [0], None, [256], [0, 256])
 
-# Read image 'image1.jpg' as grayscale
-for file in glob.glob(path):
+    # Criar a figura para exibir as imagens e seus histogramas
+    plt.figure(figsize=(18, 12))
+
+    # Mostrar imagem original
+    plt.subplot(3, 2, 1)
+    plt.imshow(img, cmap='gray')
+    plt.title('Imagem Original')
+    plt.axis('off')
+
+    # Histograma da imagem original
+    plt.subplot(3, 2, 2)
+    plt.plot(hist_img, color='black')
+    plt.title('Histograma - Imagem Original')
+    plt.xlim([0, 255])
+    plt.gca().set_ylim(bottom=0)
+
+    # Mostrar imagem equalizada
+    plt.subplot(3, 2, 3)
+    plt.imshow(img_eq, cmap='gray')
+    plt.title('Imagem Equalizada')
+    plt.axis('off')
+
+    # Histograma da imagem equalizada
+    plt.subplot(3, 2, 4)
+    plt.plot(hist_img_eq, color='blue')
+    plt.title('Histograma - Imagem Equalizada')
+    plt.xlim([0, 255])
+    plt.gca().set_ylim(bottom=0)
+
+    # Mostrar imagem invertida
+    plt.subplot(3, 2, 5)
+    plt.imshow(img_inverted, cmap='gray')
+    plt.title('Imagem Invertida')
+    plt.axis('off')
+
+    # Histograma da imagem invertida
+    plt.subplot(3, 2, 6)
+    plt.plot(hist_img_inverted, color='red')
+    plt.title('Histograma - Imagem Invertida')
+    plt.xlim([0, 255])
+    plt.gca().set_ylim(bottom=0)
+
+    # Ajustar o layout
+    plt.tight_layout()
+    plt.show()
+
+def process_image(file):
+    # Ler imagem em escala de cinza
     img = cv2.imread(file, cv2.IMREAD_GRAYSCALE)
-
     img_inverted = 255 - img
-
-    # Equalizar o histograma
     img_eq = cv2.equalizeHist(img)
 
-    imageArr.append((img, img_eq, img_inverted))
+    return img, img_eq, img_inverted
 
-# Criar a figura para exibir as imagens
-plt.figure(figsize=(15, 10))  # Ajuste o tamanho conforme necessário
+def main():
+    # Solicitar o nome do arquivo ao usuário
+    image_name = input("Digite o nome da imagem (ou pressione Enter para processar todas as imagens): ").strip()
 
-# Loop para exibir as imagens e suas edições
-plt.subplot(len(imageArr), 3, 1)
-plt.title('Imagem Original')
-plt.subplot(len(imageArr), 3, 2)
-plt.title('Imagem Equalizada')
-plt.subplot(len(imageArr), 3, 3)
-plt.title('Imagem Invertida')
+    # Verificar se o nome do arquivo foi inserido
+    if image_name:
+        image_path = os.path.join('./images', image_name)
 
-for i, tup in enumerate(imageArr):
-    img, img_eq, img_inverted = tup
+        if os.path.exists(image_path):
+            # Processar a imagem especificada
+            img, img_eq, img_inverted = process_image(image_path)
 
-    # Mostrar original
-    plt.subplot(len(imageArr), 3, 3 * i + 1)
-    plt.imshow(img, cmap='gray')
-    
-    plt.axis('off')
+            # Exibir a imagem e seus histogramas
+            plot_image_and_histogram(img, img_eq, img_inverted)
+        else:
+            print("Imagem não encontrada no diretório './images'.")
+    else:
+        # Processar todas as imagens na pasta
+        for file in glob.glob('./images/*'):
+            img, img_eq, img_inverted = process_image(file)
 
-    # Mostrar equalizada
-    plt.subplot(len(imageArr), 3, 3 * i + 2)
-    plt.imshow(img_eq, cmap='gray')
-    
-    plt.axis('off')
+            # Exibir a imagem e seus histogramas para cada imagem
+            plot_image_and_histogram(img, img_eq, img_inverted)
 
-    # Mostrar invertida
-    plt.subplot(len(imageArr), 3, 3 * i + 3)
-    plt.imshow(img_inverted, cmap='gray')
-    
-    plt.axis('off')
-
-# Exibir todas as imagens
-plt.tight_layout()  # Melhora o espaçamento entre os subplots
-plt.show()
+if __name__ == '__main__':
+    main()
